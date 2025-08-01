a7bc441766abc746f04a419efdcdb70e7893e675
Abhishek Kumar Patel <abhixpatel2002@gmail.com>
2025-08-01 20:51:59 +0530

    exercise3.py

--- Day 2/exercise3.py
+++ Day 2/exercise3.py
@@ -8,6 +8,16 @@ You have x days, y weeks, and z months left.
 Where x, y and z are replaced with the actual calculated numbers.
 '''
 
+'''
+Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+
+It will take your current age as the input and output a message with our time left in this format:
+
+You have x days, y weeks, and z months left.
+
+Where x, y and z are replaced with the actual calculated numbers.
+'''
+
 # 🚨 Don't change the code below 👇
 age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
@@ -15,14 +25,10 @@ age = input("What is your current age?")
 # Write your code below this line 👇
 
 try:
-    age = int(age)
-    years_left = 90 - age
-    months_left = years_left * 12
-    weeks_left = years_left * 52
-    days_left = years_left * 365
-
-    print(
-        f"You have {years_left} years, {months_left} months, {weeks_left} weeks and {days_left} days left.")
-
+    a = 90-int(age)
+    x = a*365
+    y = a*52
+    z = a*12
+    print("You have", x,"days", y,"weeks", "and",z ,"months left")
 except ValueError:
-    print("Enter age in integer only.")
+    print("please enter age value in Integer")
