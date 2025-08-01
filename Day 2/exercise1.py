aa62cac0490c9834c7fa958fa3f32f22599bab95
Abhishek Kumar Patel <abhixpatel2002@gmail.com>
2025-08-01 20:30:58 +0530

    exercise1.py

--- Day 2/exercise1.py
+++ Day 2/exercise1.py
@@ -11,4 +11,4 @@ try:
     sum = int(two_digit_number[0])+int(two_digit_number[1])
     print(f"Sum of digits of {two_digit_number} is {sum}.")
 except ValueError:
-    print("Please enter an integer.")
+    print("Please enter only two digit integer.")
