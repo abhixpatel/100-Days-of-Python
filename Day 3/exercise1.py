32f1a5243ca438385a8af83511c09fe19bf86c6b
Abhishek Kumar Patel <abhixpatel2002@gmail.com>
2025-08-04 17:39:20 +0530

    exercise1.py

--- Day 3/exercise1.py
+++ Day 3/exercise1.py
@@ -2,4 +2,11 @@
 Write a program that works out whether if a given number is an odd or even number.
 '''
 
-print("Even") if int(input()) % 2 == 0 else print("Odd")
+try:
+    sum = int(input("enter the no. to check: "))
+    if sum %2 ==0:
+        print(f"the no.{sum} is even")
+    else :
+        print(f"the no.{sum} is Odd")
+except ValueError:
+    print("please enter integer value only")
