0d58a701fc88b9301e6c4470e7df39f4f8aa1506
Abhishek Kumar Patel <abhixpatel2002@gmail.com>
2025-08-05 17:37:15 +0530

    Update exercise2.py

--- Day 4/exercise2.py
+++ Day 4/exercise2.py
@@ -3,20 +3,17 @@ You are going to write a program which will select a random name from a list of 
 '''
 
 import random
+
 # 🚨 Don't change the code below 👇
 test_seed = int(input("Create a seed number: "))
 random.seed(test_seed)
 
 # Split string method
-namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
-names = namesAsCSV.split(", ")
+namesAsCSV = input("Give me everybody's names, separated by a comma: ")
+names = namesAsCSV.split(",")
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
-num_items = len(names)
-# Generate random numbers between 0 and the last index.
-random_choice = random.randint(0, num_items - 1)
-# Pick out random person from list of names using the random number.
-person_who_will_pay = names[random_choice]
+payer = random.choice(names)
 
-print(person_who_will_pay + " is going to buy the meal today!")
+print(f"{payer} is going to buy the meal today!️")
