f0c4d9c5d23fe8fd10d6dd6278936680d09b4e59
Abhishek Kumar Patel <abhixpatel2002@gmail.com>
2025-08-05 19:56:08 +0530

    Update game.py

--- Day 4/game.py
+++ Day 4/game.py
@@ -1,54 +1,30 @@
-'''
-Rock Paper Scissor
-'''
+# ROCK PAPER SCISSOR game
 
 import random
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-
-game_images = [rock, paper, scissors]
-
-user_choice = int(
-    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[user_choice])
-
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])
-
-if user_choice >= 3 or user_choice < 0:
-    print("You typed an invalid number, you lose!")
-elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-    print("You lose")
-elif computer_choice > user_choice:
-    print("You lose")
-elif user_choice > computer_choice:
-    print("You win!")
-elif computer_choice == user_choice:
-    print("It's a draw")
+list = ["Rock","Paper","Scissors"]
+user = int(input("Choose your turn (0, 1, 2 for rock, paper, scissor respectively): "))
+print(f"you chose {list[user]}")
+com_choice = random.randint(0,2)
+print(f"the computer chose {list[com_choice]}")
+if user == 0:
+    if com_choice == 0:
+        print("Draw")
+    elif com_choice ==1:
+        print("you lost")
+    else:
+        print("you won")
+elif user == 1:
+    if com_choice == 1:
+        print("Draw")
+    elif com_choice ==2:
+        print("you lost")
+    else:
+        print("you won")
+        
+else:
+    if com_choice == 2:
+        print("Draw")
+    elif com_choice ==0:
+        print("you lost")
+    else:
+        print("you won")
