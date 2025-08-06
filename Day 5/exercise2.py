6b4537853b42da03c9a4112d3a8dbbb4814c987a
Abhishek Kumar Patel <abhixpatel2002@gmail.com>
2025-08-06 15:27:28 +0530

    Update exercise2.py
    i used value based loop approach

--- Day 5/exercise2.py
+++ Day 5/exercise2.py
@@ -9,18 +9,13 @@ The highest score in the class is: x
 '''
 
 # 🚨 Don't change the code below 👇
-student_scores = input("Input a list of student scores ").split()
+student_scores = input("Input a list of student scores: ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
-print(student_scores)
-# 🚨 Don't change the code above 👆
 
-# Write your code below this row 👇
-
-max_score = 0
-
+highest = 0
 for score in student_scores:
-    if score > max_score:
-        max_score = score
+    if score > highest:
+        highest = score
 
-print(f"The highest score in the class is: {max_score}")
+print(f"The highest score in the class is: {highest}")
