1b63354859bb18b141374181479d3f9c31e01ebf
Abhishek Kumar Patel <abhixpatel2002@gmail.com>
2025-08-08 12:48:59 +0530

    Update challenge1.py
    for loop is necessary

--- Day 7/challenge1.py
+++ Day 7/challenge1.py
@@ -5,16 +5,15 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-chosen_word = random.choice(word_list)
+chosen = random.choice(word_list)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter: ").lower()
+guess = input("Guess a letter among the word_list: ").lower()
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for letter in chosen_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
+if guess == chosen:
+    print("you guessed Right")
+else:
+    print("you guessed Wrong")
